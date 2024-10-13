f374979bd45854839bde1959563fbf03375c09e3
Stefan <stefancool00@gmail.com>
2024-10-13 11:38:45 -0700

    Typo corrected

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -5,7 +5,7 @@ export const NavBar: React.FC = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl"><Link to={'/'}> My Books </Link></a>
+                <a className="btn btn-ghost text-xl"><Link to={'/'}> eLibrary </Link></a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
