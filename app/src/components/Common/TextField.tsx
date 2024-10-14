4c53e5623fddb289ec3001bbba3c583c40d884df
Stefan <stefancool00@gmail.com>
2024-10-13 22:35:09 -0700

    Added title to form and save button, some frontend styling

---
+++ app/src/components/Common/TextField.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FieldInputProps, FormikProps } from "formik";
+
+interface Props {
+    field: FieldInputProps<any>;
+    form: FormikProps<any>;
+    placeholder?: string;
+}
+
+export const TextField: React.FC<Props> = ({ field, form, placeholder }) => {
+    return (
+        <input 
+            type="text" 
+            className="input input-bordered w-full max-w-xl" 
+            {...field}  // Pass formik's field props to the input
+            placeholder={placeholder} // Optionally pass the placeholder
+        />
+    )
+}
--- app/src/components/Forms/NewBookForm.tsx
+++ app/src/components/Forms/NewBookForm.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { TextField } from "../Common/TextField";
 
 interface IValue {
-    description: string;
+    title: string;
 }
 
 const validationSchema = Yup.object({
-    description: Yup.string()
-        .required("Description is required")
-        .min(10, "Description should be at least 10 characters")
+    title: Yup.string()
+        .required("Title is required")
+        .min(3, "Title should be at least 3 characters")
 });
 
 export const NewBookForm: React.FC = () => {
     const ipcRenderer = (window as any).ipcRenderer
 
     const initialValues: IValue = {
-        description: ''
+        title: ''
     };
 
     const onSubmit = (values: IValue) => {
@@ -31,10 +32,20 @@ export const NewBookForm: React.FC = () => {
                 validationSchema={validationSchema}
                 onSubmit={onSubmit}
             >
-                <Form>
-                    <Field name="description" id="description" type="text" />
-                    <ErrorMessage name="description"/>
-                    <button type="submit">Save</button>
+                <Form className="shadow border rounded-xl p-4 my-4 flex flex-col items-center">
+                    <Field 
+                        name="title" 
+                        component={TextField}
+                        placeholder="Enter Title of your Book"
+                    />
+                    <ErrorMessage 
+                        name="title"
+                        component="span"
+                        className="text-red-600 text-sm"
+                    />
+                    <div className="m-2">
+                        <button type="submit" className="btn btn-primary"> Save New Book </button>
+                    </div>
                 </Form>
             </Formik>
         </div>
